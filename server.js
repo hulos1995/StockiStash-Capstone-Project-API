c0ee50a9daa83b80a264257c55287714c8820c9a
Bao <bao9600455@yahoo.com.vn>
2024-06-26 22:36:46 -0400

    get all inventory, id and filter null grits

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import inventory from "./routes/inventory.js";
 const app = express();
 const { CORS_URL } = process.env;
 app.use(express.json());
 app.use(cors({ origin: CORS_URL }));
 const PORT = process.env.PORT || 5173;
 app.use(express.static("public"));
+app.use("/inventory", inventory);
 app.listen(PORT, function () {
   console.log(`App is listening on port ${PORT}`);
 });
