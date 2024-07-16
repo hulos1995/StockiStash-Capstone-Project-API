2cf2d273c73dd1be831eed197fd9b7d2a38e47d7
Bao <bao9600455@yahoo.com.vn>
2024-07-16 12:01:10 -0400

    Update CORS policy to dynamically set Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -7,11 +7,26 @@ import signup from './routes/signup.js';
 import profile from './routes/profile.js';
 import cart from './routes/cart.js';
 import notifications from './routes/notifications.js'; 
+
 const app = express();
 const { CORS_URL } = process.env;
+const allowedOrigins = CORS_URL.split(' ');
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
 app.use(express.json());
-app.use(cors({ origin: CORS_URL }));
-const PORT = process.env.PORT || 5173;
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 app.get("/", (_req, res) => {
   res.send("Welcome to my API");
@@ -23,6 +38,7 @@ app.use('/signup', signup);
 app.use('/profile', profile);
 app.use('/cart', cart);
 app.use('/notifications', notifications);
+
 app.listen(PORT, function () {
   console.log(`App is listening on port ${PORT}`);
 });
